b7559d4b591d5d1846da70fde42e9ce403860d82
Luiz Maciel <52587596+luiz-dos@users.noreply.github.com>
2024-06-04 16:24:53 +0100

    Update test_bonus.c

--- test_bonus.c
+++ test_bonus.c
@@ -3,33 +3,39 @@
 
 int main(void)
 {
-    int     fd;
+    int     fd1;
     int     fd2;
     int     fd3;
     char    *result;
     char    *result2;
     char    *result3;
 
-    fd = open("t1.txt", O_RDWR);
+    fd1 = open("t1.txt", O_RDWR);
     fd2 = open("t2.txt", O_RDWR);
     fd3 = open("t3.txt", O_RDWR);
 
     while (1)
     {
-        result = get_next_line(fd);
+        result = get_next_line(fd1);
         if (result)
+        {
             printf("%s", result);
+            free (result);
+        }
         result2 = get_next_line(fd2);
         if (result2)
+        {
             printf("%s", result2);
+            free (result2);
+        }
         result3 = get_next_line(fd3);
         if (result3)
+        {
             printf("%s", result3);
+            free (result3);
+        }
         if (!result && !result2 && !result3)
             break ;
-        free (result);
-        free (result2);
-        free (result3);
     }
     return (0);
 }
