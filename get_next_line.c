8f1b6a42f822c3a5d0a056fd325f006aaa86d2d5
Luiz Maciel <52587596+luiz-dos@users.noreply.github.com>
2024-06-04 12:13:15 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luiz-dos <luiz-dos@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 11:13:55 by luiz-dos          #+#    #+#             */
+/*   Updated: 2024/06/04 11:13:56 by luiz-dos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char buffer[BUFFER_SIZE + 1];
-    char        *line;
-    int         found_newline;
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*line;
+	int			found_newline;
 
-    if (read(fd, 0, 0) < 0 || fd >= FOPEN_MAX || fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    line = NULL;
-    found_newline = 0;
-    while(!found_newline && (buffer[0] || read(fd, buffer, BUFFER_SIZE) > 0))
-    {
-        line = ft_line(line, buffer);
-        ft_handle_buffer(buffer, &found_newline);
-    }
-    return (line);
+	if (read(fd, 0, 0) < 0 || fd >= FOPEN_MAX || fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = NULL;
+	found_newline = 0;
+	while (!found_newline && (buffer[0] || read(fd, buffer, BUFFER_SIZE) > 0))
+	{
+		line = ft_line(line, buffer);
+		ft_handle_buffer(buffer, &found_newline);
+	}
+	return (line);
 }
