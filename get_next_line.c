1909a1cac190be1f1f098f43287229dd8eafe708
Luiz Maciel <52587596+luiz-dos@users.noreply.github.com>
2024-06-06 15:59:14 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,20 +12,29 @@
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+/*
+* check if BUFFER_SIZE > INT_MAX
+* check if the file can be opened
+* check if it has not reached FOPEN_MAX
+* check if fd < 0
+* check if BUFFER_SIZE <= 0
+* read the file, create a line, put the rest in the static variable for the next call
+* while the line is not created, continue reading (put a flag)
+*/
+char    *get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
-	char		*line;
-	int			found_newline;
+    static char buffer[BUFFER_SIZE];
+    char        *line;
+    int         found_newline;
 
-	if (read(fd, 0, 0) < 0 || fd >= FOPEN_MAX || fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	line = NULL;
-	found_newline = 0;
-	while (!found_newline && (buffer[0] || read(fd, buffer, BUFFER_SIZE) > 0))
-	{
-		line = ft_line(line, buffer);
-		ft_handle_buffer(buffer, &found_newline);
-	}
-	return (line);
+    if (BUFFER_SIZE > 2147483647 || read(fd, 0, 0) < 0 || fd >= FOPEN_MAX || fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    line = NULL;
+    found_newline = 0;
+    while(!found_newline && (buffer[0] || read(fd, buffer, BUFFER_SIZE) > 0))
+    {
+        line = ft_line(line, buffer);
+        ft_handle_buffer(buffer, &found_newline);
+    }
+    return (line);
 }
