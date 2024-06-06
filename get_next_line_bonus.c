41b8822e28757e9762000459eae153c0a9029554
Luiz Maciel <52587596+luiz-dos@users.noreply.github.com>
2024-06-06 16:01:01 +0100

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -12,21 +12,23 @@
 
 #include "get_next_line_bonus.h"
 
-char	*get_next_line(int fd)
+/* The difference is that now I specify the file descriptor that is being 
+processed, meaning that in each call my auxiliary functions receive 
+the buffer from the specific file descriptor. */
+char    *get_next_line(int fd)
 {
-	static char	buffer[FOPEN_MAX][BUFFER_SIZE + 1];
-	char		*line;
-	int			found_newline;
+    static char buffer[FOPEN_MAX][BUFFER_SIZE];
+    char        *line;
+    int         found_newline;
 
-	if (read(fd, 0, 0) < 0 || fd >= FOPEN_MAX || fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	line = NULL;
-	found_newline = 0;
-	while (!found_newline && (buffer[fd][0] || read(fd, buffer[fd],
-				BUFFER_SIZE) > 0))
-	{
-		line = ft_line(line, buffer[fd]);
-		ft_handle_buffer(buffer[fd], &found_newline);
-	}
-	return (line);
+    if (BUFFER_SIZE > 2147483647 || read(fd, 0, 0) < 0 || fd >= FOPEN_MAX || fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    line = NULL;
+    found_newline = 0;
+    while(!found_newline && (buffer[fd][0] || read(fd, buffer[fd], BUFFER_SIZE) > 0))
+    {
+        line = ft_line(line, buffer[fd]);
+        ft_handle_buffer(buffer[fd], &found_newline);
+    }
+    return (line);
 }
